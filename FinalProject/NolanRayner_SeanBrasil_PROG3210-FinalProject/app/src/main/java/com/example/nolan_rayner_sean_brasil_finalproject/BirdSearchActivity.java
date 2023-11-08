5c6afc70e51fa3a2e4cc59e3d2c8cf5eccfe46e6
Nolan-Git <147003017+Nolan-Git@users.noreply.github.com>
2023-11-08 18:30:55 -0500

    Add files via upload
    Initial Upload of the Mobile Project

---
+++ FinalProject/NolanRayner_SeanBrasil_PROG3210-FinalProject/app/src/main/java/com/example/nolan_rayner_sean_brasil_finalproject/BirdSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.nolan_rayner_sean_brasil_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BirdSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bird_search);
+    }
+}
---
+++ FinalProject/NolanRayner_SeanBrasil_PROG3210-FinalProject/app/src/main/java/com/example/nolan_rayner_sean_brasil_finalproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nolan_rayner_sean_brasil_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
