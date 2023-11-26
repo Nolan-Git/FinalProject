21428a19d1b329142d94a655f463340ab8385b57
Nolan-Git <147003017+Nolan-Git@users.noreply.github.com>
2023-11-26 18:02:28 -0500

    Update AboutActivity.java
    added java for about page

--- FinalProject/NolanRayner_SeanBrasil_PROG3210-FinalProject/app/src/main/java/com/example/nolan_rayner_sean_brasil_finalproject/AboutActivity.java
+++ FinalProject/NolanRayner_SeanBrasil_PROG3210-FinalProject/app/src/main/java/com/example/nolan_rayner_sean_brasil_finalproject/AboutActivity.java
@@ -3,6 +3,9 @@ package com.example.nolan_rayner_sean_brasil_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.net.Uri;
+import android.content.Intent;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void btnBirdCanada_click(View view){
+        goToUrl ( "https://www.birdscanada.org/");
+    }
+
+    public void btnNatGeo_click(View view){
+        goToUrl ( "https://www.nationalgeographic.com/animals/birds");
+    }
+
+    public void btnAAB_click(View view){
+        goToUrl ( "https://www.allaboutbirds.org/news/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
